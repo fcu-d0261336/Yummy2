28b90bb37d38153b1d86a844a6f877d3ae9227f8
d0261336 <yiyanziying@163.com>
2017-05-24 20:55:37 +0800

    webview came true!!! could just use this!!!

--- app/src/main/java/com/example/niu/yummy/CuisineActivity2.java
+++ app/src/main/java/com/example/niu/yummy/CuisineActivity2.java
@@ -3,7 +3,10 @@ package com.example.niu.yummy;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 
@@ -17,7 +20,30 @@ public class CuisineActivity2 extends AppCompatActivity {
         main_button.setOnClickListener(back_main);
         Button res_button=(Button)findViewById(R.id.res_btn);
         res_button.setOnClickListener(res);
+/////////////////////////////////////////////////////////////////////////////////////////////
+        //WebView
+        WebView browser=(WebView)findViewById(R.id.Toweb);
+        browser.loadUrl("http://mp.weixin.qq.com/s/qVDQb6DcWngbWTHoL6siNA");
+
+        //设置可自由缩放网页
+        browser.getSettings().setSupportZoom(true);
+        browser.getSettings().setBuiltInZoomControls(true);
+
+        // 如果页面中链接，如果希望点击链接继续在当前browser中响应，
+        // 而不是新开Android的系统browser中响应该链接，必须覆盖webview的WebViewClient对象
+        browser.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            {
+                //  重写此方法表明点击网页里面的链接还是在当前的webview里跳转，不跳到浏览器那边
+                view.loadUrl(url);
+                return true;
+            }
+        });
+/////////////////////////////////////////////////////////////////////////////////////////////
+
     }
+
+
     private OnClickListener back_main=new OnClickListener() {
         @Override
         public void onClick(View view) {Intent intent=new Intent();
@@ -35,5 +61,20 @@ public class CuisineActivity2 extends AppCompatActivity {
             startActivity(intent);
         }
     };
-
+//////////////////////////////////////////////////////////////////////////////////////////////
+    //go back
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        WebView browser=(WebView)findViewById(R.id.Toweb);
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && browser.canGoBack()) {
+            browser.goBack();
+            return true;
+        }
+        //  return true;
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+///////////////////////////////////////////////////////////////////////////////////////////////
 }
